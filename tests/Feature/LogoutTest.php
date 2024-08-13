8b5fbce567a6bea1529dd6034f458aa81b28888a
Daniella Brito Côrtes <danibcortes@hotmail.com>
2024-08-13 16:15:47 -0300

    Teste Logout com falha

--- tests/Feature/LogoutTest.php
+++ tests/Feature/LogoutTest.php
@@ -7,25 +7,49 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
-
+use Illuminate\Support\Arr;
 class LogoutTest extends TestCase
 {
     public function testUserIsLoggedOutProperly()
     {
-        // $faker = \Faker\Factory::create();
+        $faker = \Faker\Factory::create();
 
-        // $user = User::factory() ->create(['email' => $faker->email]);
-        // $token = $user->generateToken();
-        // $headers = ['Authorization' => "Bearer $token"];
+        $user = User::factory() ->create(['email' => $faker->email]);
+        
+        $user->createToken('access_token');
+        $array = $user->tokens[0];
 
-        Sanctum::actingAs(
-            $user = User::factory()->create(),
-            ['view-tasks']
-        );
+        $token = Arr::get($array, 'token');
 
-        $this->json('get', '/api/articles', [])->assertStatus(200);
-        $this->json('post', '/api/logout', [])->assertStatus(202);
+        dd($token);        
 
-        $this->assertEquals(null, $user->api_token);
+        $headers = ['Authorization' => "Bearer $token"];
+
+        $this->json('get', '/api/articles', [], $headers)->assertStatus(200);
+        $this->json('post', '/api/logout', [], $headers)->assertStatus(202);
+        $this->json('get', '/api/articles', [], $headers)->assertStatus(401);
+        
+        $this->assertEquals(true, $user->tokens->isEmpty());
+
+        // dd($user->tokens);
+
+        // $this->assertNotEmpty($user->currentAccessToken());
     }
+
+    // public function testUserWithNullToken()
+    // {
+    //     // // Simulating login
+    //     // $user = factory(User::class)->create(['email' => 'user@test.com']);
+    //     // $token = $user->generateToken();
+    //     // $headers = ['Authorization' => "Bearer $token"];
+
+    //     Sanctum::actingAs(
+    //         $user = User::factory()->create(),
+    //         ['view-tasks']
+    //     );
+
+    //     $user->user()->currentAccessToken()->delete();
+
+    //     $this->json('post', '/api/logout', [])->assertStatus(401);
+    // }
 }
